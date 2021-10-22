1ef9679a5027453f40f030b02a23e8ed0ab0b7d2
Арслан Арифуллин <arifarsrad@gmail.com>
2021-10-22 12:30:54 +0300

    fix bug with navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,6 @@ import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import {Link} from "@mui/material";
 
 
@@ -11,12 +10,9 @@ export default function NavBar() {
     return (
         <AppBar position="static">
             <Toolbar>
-                <Link href='/catalogue' sx={{flexGrow: 1}}>
-                    <Typography variant="h6" component="div" color='white'>
-                        DrugSearcher
-                    </Typography>
-                </Link>
-                <Link href='/cart'><Typography color='white'><ShoppingCartIcon/></Typography></Link>
+                <Typography variant="h6" component="div" color='inherit' sx={{mr: 2}}>DrugSearcher</Typography>
+                <Link href='/cart' color='inherit' underline='none' sx={{mr: 2}}>Cart</Link>
+                <Link href='/catalogue' color='inherit' underline='none'>Catalogue</Link>
             </Toolbar>
         </AppBar>
     );
